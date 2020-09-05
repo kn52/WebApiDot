e7fd305071a9162373bd74f18dd1084f0172aed9
kn52 <ashish52922@gmail.com>
2020-09-05 17:37:11 +0530

    [Aashish]ADDED:Json web token Authentication

--- EmpLayer/TokenGenerator.cs
+++ EmpLayer/TokenGenerator.cs
@@ -7,6 +7,49 @@
     using System.Text;
     public class TokenGenerator
     {
-        
+        public static string GenerateJSONWebToken(IConfiguration configuration)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(configuration["Jwt:Issuer"],
+              configuration["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public static bool ValidateJSONWebToken(string token, IConfiguration configuration)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = GetValidationParameters(configuration);
+            try
+            {
+                SecurityToken validatedToken = tokenHandler.ReadToken(token);
+                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+            return true;
+
+        }
+
+        private static TokenValidationParameters GetValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["Jwt:Issuer"],
+                ValidAudience = configuration["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:key"]))
+            };
+        }
     }
 }
--- WebApiDemo/Controllers/TokenController.cs
+++ WebApiDemo/Controllers/TokenController.cs
@@ -17,7 +17,7 @@
         [HttpGet]
         public string GetToken()
         {
-            return GenerateJSONWebToken();
+            return Service.GenerateJSONWebToken();
         }
 
         // GET api/<controller>/5
@@ -38,58 +38,13 @@
         [HttpPut]
         public bool ValidateToken([FromBody]string token)
         {
-            return ValidateJSONWebToken(token);
+            return Service.ValidateJSONWebToken(token);
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
-
-        public static string GenerateJSONWebToken(IConfiguration configuration)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(configuration["Jwt:Issuer"],
-              configuration["Jwt:Issuer"],
-              null,
-              expires: DateTime.Now.AddMinutes(120),
-              signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-        public static bool ValidateJSONWebToken(string token, IConfiguration configuration)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var validationParameters = GetValidationParameters(configuration);
-            try
-            {
-                SecurityToken validatedToken = tokenHandler.ReadToken(token);
-                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
-            }
-            catch (Exception ex)
-            {
-
-                return false;
-            }
-            return true;
-
-        }
-
-        private static TokenValidationParameters GetValidationParameters(IConfiguration configuration)
-        {
-            return new TokenValidationParameters()
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = false,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = configuration["Jwt:Issuer"],
-                ValidAudience = configuration["Jwt:Issuer"],
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:key"]))
-            };
-        }
+        }        
     }
 }
